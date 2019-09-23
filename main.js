0c7b68d2340298ed552852cca6be523439b05486
레이라 <layla1017@naver.com>
2019-09-23 22:38:51 +0900

    헬로월드를 검은 원에 띄워봅시다

--- main.js
+++ main.js
@@ -6,6 +6,13 @@ $(document).ready(function() {
   var c;
   var tool = new Tool();
 
+  var c = Shape.Circle(200, 200, 80);
+  c.fillColor = 'black';
+  var text = new PointText(200, 200);
+  text.justification = 'center';
+  text.fillColor = 'white';
+  text.fontSize = 20;
+  text.content = 'hello world';
   tool.onMouseDown = function(event) {
     var c = Shape.Circle(event.point.x, event.point.y, 20);
     c.fillColor = 'green';
