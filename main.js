9152d0ab24dede32638802c2ef5dfc7cc0a1ac82
레이라 <layla1017@naver.com>
2019-09-23 22:35:50 +0900

    클릭을 하면 원이 나오도록 입력처리를 해봅시다.

--- main.js
+++ main.js
@@ -4,12 +4,13 @@ $(document).ready(function() {
   paper.setup(document.getElementById('mainCanvas'));
 
   var c;
-  for(var x=25; x<400; x+=50) {
-    for(var y=25; y<400; y+=50) {
-      c = Shape.Circle(x, y, 20);
-      c.fillColor = 'green';
+  var tool = new Tool();
 
-    }
+  tool.onMouseDown = function(event) {
+    var c = Shape.Circle(event.point.x, event.point.y, 20);
+    c.fillColor = 'green';
+
   }
+
 
   });
